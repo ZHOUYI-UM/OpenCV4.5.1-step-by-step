6f6dc75ba0962ba87b8c5bfcf00c406c0f2c4d7f
ZHOUYI-UM <91511540+ZHOUYI-UM@users.noreply.github.com>
2022-04-26 13:40:48 +0800

    First Qt project using OpenCV to read a picture

--- Prj1/Prj1.pro
+++ Prj1/Prj1.pro
@@ -14,6 +14,8 @@ DEFINES += QT_DEPRECATED_WARNINGS
 # In order to do so, uncomment the following line.
 # You can also select to disable deprecated APIs only up to a certain version of Qt.
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
+INCLUDEPATH += D:\OpenCV4.5.1\opencv-build\install\include
+LIBS += D:\opencv4.5.1\opencv-build\lib\libopencv_*.a
 
 SOURCES += \
     main.cpp \
--- Prj1/mainwindow.cpp
+++ Prj1/mainwindow.cpp
@@ -1,5 +1,10 @@
-#include "mainwindow.h"
+﻿#include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <opencv2/opencv.hpp>
+#include <QFileDialog>
+#include <QMessageBox>
+
+using namespace cv;
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -13,3 +18,16 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::on_pbLoadPicture_clicked()
+{
+    QString strFilePic = QFileDialog::getOpenFileName(this, tr("Open picture"));
+    if (strFilePic.isEmpty())
+    {
+        QMessageBox::information(this, tr("Warning"), tr("No file selected!"));
+        return ;
+    }
+    //注意：imread不支持中文名字
+    cv::Mat img = imread(strFilePic.toLatin1().data(), 1);//一定要使用绝对路径，其他可以回报错
+    namedWindow(strFilePic.toLatin1().data(), WINDOW_AUTOSIZE);
+    imshow(strFilePic.toLatin1().data(), img);
+}
